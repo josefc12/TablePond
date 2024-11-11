437ac0927fafacb1691bbb5f16df99a636919958
Josef Cermak <josef.cermak@miele.com>
2024-11-11 14:44:24 +0100

    small progress

--- PepeWeb/Data/ApplicationUser.cs
+++ PepeWeb/Data/ApplicationUser.cs
@@ -5,6 +5,19 @@ namespace PepeWeb.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string Nickname
+        {
+            get
+            {
+                // Get the part before the '@' symbol in the email
+                if (UserName != null)
+                {
+                    var atIndex = UserName.IndexOf('@');
+                    return atIndex > 0 ? UserName.Substring(0, atIndex) : UserName;
+                }
+                return string.Empty;
+            }
+        }
     }
 
 }
